cb508796fc336be6b917224862340bac62f3eff9
PatrickLeGarrec <patrick.legarrec.pro@gmail.com>
2024-07-02 21:54:16 +0200

    new version

---
+++ ConsoleApp/NUnitExplorer.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Engine;
+using System.Xml;
+
+namespace NUnitWrappers;
+
+public class NUnitExplorer
+{
+    string _dll, _workSpaceDir, _archiveDir;
+
+    public NUnitExplorer(string dll)
+    {
+        _dll = dll;
+        _workSpaceDir = AppDomain.CurrentDomain.BaseDirectory.Replace(@"ConsoleApp\bin\Debug\net8.0", "WorkSpace");
+        _archiveDir = AppDomain.CurrentDomain.BaseDirectory.Replace(@"ConsoleApp\bin\Debug\net8.0","Archive");
+    }
+
+    public void Explore()
+    {
+        try
+        {
+            File.Copy(_workSpaceDir + _dll, AppDomain.CurrentDomain.BaseDirectory + _dll, true);
+
+            ITestEngine engine = TestEngineActivator.CreateInstance();
+            engine.InternalTraceLevel = InternalTraceLevel.Off;
+            TestPackage package = new TestPackage(_dll);
+            ITestRunner runner = engine.GetRunner(package);
+            XmlNode explore = runner.Explore(TestFilter.Empty);         
+                                                                        
+            Console.WriteLine(explore.OuterXml.ToString());
+
+            runner.Unload();
+            runner.Dispose();
+            engine.Dispose();
+        }
+        catch (Exception ex) { Console.WriteLine("\n" + ex.ToString()); }
+    }
+
+    public void Archive()
+    {
+        try
+        {
+            File.Copy(_workSpaceDir + _dll, _archiveDir + "explored_" + _dll, true);
+            File.Delete(AppDomain.CurrentDomain.BaseDirectory + _dll);    // file is Locked
+                                                                          
+        } catch (Exception ex) { Console.WriteLine("\n" + ex.ToString()); }
+    }
+}
+
+// https://docs.nunit.org/articles/nunit-engine/Test-Engine-API.html
+
+// https://docs.nunit.org/articles/vs-test-adapter/Adapter-Engine-Compatibility.html
+
+// https://docs.nunit.org/articles/nunit/release-notes/Nunit4.0-MigrationGuide.html
+
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using NUnitWrappers;
+
+
+NUnitExplorer nUnitExplorer = new("Test.dll");
+
+nUnitExplorer.Explore();
+
+nUnitExplorer.Archive();
+
+
+
+
+//   How this proof of concept works  : 
+/*
+ * 
+ *   The idea is to have a workspace where the fresh tests to be explored (and executed later) are forged in Tests.dll.
+ *   the Test App execute a test and copy its own dll to the workspace. by doing this, it s easier to check the libs compatibility.
+ *   
+ *   The consoleApp take the test from the workspace, import it into the NUnit workingDir (where the dependencies are), and does the nUnit engine stuff as usual.
+ *   
+ *   After the exploration has been processed, the Test.dll is archived in the archive folder, and of course deleted from the NUnit workingDir. 
+ *   
+ *    
+ *   I got many issues with dependencies versions and the winning combos are : 
+ *   
+ *   For the Test :
+ *   Net.test.sdk  17.10.0
+ *   NUnit 3.14.0
+ *   Nunit3TestAdapter 4.3.0
+ *   
+ *   For the ConsoleApp (nUnit engine stuff) :
+ *   Net.test.sdk 17.10.0
+ *   NUnit 3.14.0
+ *   NUnit Engine 3.14.0
+ *   
+ *   maybe i could be successull with earlier versions but most of the others combos have failed. (File not found during engine.explore)
+ * 
+ *   with those libs the Test.dll is correcly read. (without the mess of the 3.17 engine version / hypothetic dependency on the Test.dll itself like as described, forget that.)
+ *   
+ *   But the file is still locked when deleting.
+ *   
+ * 
+ *   Patrick.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
